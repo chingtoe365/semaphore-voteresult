060f17b2660db551402c476439124f5961a90bbc
chingtoe365 <vagrant@precise64.(none)>
2017-07-04 14:11:03 +0000

    first commit

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request, make_response, g
+from redis import Redis
+import os
+import socket
+import random
+import json
+
+app = Flask(__name__)
+
+def get_redis():
+    if not hasattr(g, 'redis'):
+        g.redis = Redis(host="redis", db=0, socket_timeout=5)
+    return g.redis
+
+@app.route("/", methods=['POST','GET'])
+def hello():
+    redis = get_redis()
+    raw_data = json.dumps(redis.get('votes'))
+    resp = make_response(render_template(
+        'index.html',
+        raw_data=raw_data
+    ))
+    return resp
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80, debug=True, threaded=True)
