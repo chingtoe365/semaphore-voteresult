8fcdfdcacefb7bb3ac3f177b89de59e53c2f8c13
chingtoe365 <vagrant@precise64.(none)>
2017-07-07 12:26:09 +0000

    result changed content

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+# bash
+cd ~/semaphore/voteresult
+sudo docker build -t semaphore/viewresult .
+if [ ! "$(docker ps -a | grep result)" ]; then
+	sudo docker stop result
+fi
+sudo docker run --name result -d -p 8884:80 --link redis:redis semaphore/viewresult
