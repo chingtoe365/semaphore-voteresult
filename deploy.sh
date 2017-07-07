e08aaca913f44120e9d3b882f3244ede937dd832
chingtoe365 <vagrant@precise64.(none)>
2017-07-07 12:40:55 +0000

    change path in bash

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 # bash
 cd ~/semaphore/voteresult
 sudo docker build -t semaphore/show .
-if [ ! "$(docker ps -a | grep result)" ]; then
+if [ "$(docker ps -a | grep result)" ]; then
 	sudo docker stop result
 fi
 sudo docker run --name result -d -p 8884:80 --link redis:redis semaphore/viewresult
